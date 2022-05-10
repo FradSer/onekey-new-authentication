21be5a6307e1ec51c05b8b48136360afa53ad0db
Frad LEE <fradser@gmail.com>
2022-05-11 02:57:10 +0800

    refactor: ready to new feat

---
+++ configs/animation.ts
@@ -0,0 +1,13 @@
+const baseSpringTranstion = {
+  type: 'spring',
+  stiffness: 32,
+}
+
+const baseEaseTranstion = {
+  type: 'tween',
+  ease: 'easeInOut',
+}
+
+const DELAY = 0.8
+
+export { baseSpringTranstion, baseEaseTranstion, DELAY }
